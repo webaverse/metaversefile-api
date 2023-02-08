f2d867ac2e646e71274cc09915f24e937cbe9046
Avaer Kazmer <a@modules.io>
2023-02-08 04:11:11 -0500

    Add initial api

---
+++ api/compiler-base-url.js
@@ -0,0 +1,3 @@
+// https://cdn.jsdelivr.net/gh/webaverse/metaversefile-api@main/compiler-base-url.js
+import {compilerBaseUrl} from './endpoints.js';
+console.log('got compilerBaseUrl', compilerBaseUrl);
